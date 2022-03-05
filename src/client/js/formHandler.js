d88e65b99bd5ebdef8a5d5ead67f42d4f1dacae1
Rahul Raj Miglani <rahul.cad2019@gmail.com>
2022-03-05 00:14:38 -0800

    commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-function handleSubmit(event) {
+export function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -13,4 +13,3 @@ function handleSubmit(event) {
     })
 }
 
-export { handleSubmit }
